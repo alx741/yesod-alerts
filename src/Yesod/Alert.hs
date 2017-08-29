fb3a40476993d1f4642a4c76456e982a56c4ffb6
Daniel Campoverde <alx@sillybytes.net>
2017-08-29 17:04:10 -0500

    Add set shortcuts

--- src/Yesod/Alert.hs
+++ src/Yesod/Alert.hs
@@ -1,4 +1,26 @@
-module Yesod.Alert where
+-- | Set alerts from your handlers
+--
+-- @
+--      getHomeR :: Handler Html
+--      getHomeR = do
+--          ...
+--          setAlert
+-- @
+
+module Yesod.Alert
+    ( -- * Setting alerts
+      setAlert
+
+      -- ** Shortcuts
+    , setDefaultAlert
+    , setInfoAlert
+    , setSuccessAlert
+    , setWarningAlert
+    , setErrorAlert
+
+    -- * Getting alerts
+    , getAlerts
+    ) where
 
 import Data.Maybe (fromMaybe)
 import Data.Text
@@ -6,11 +28,26 @@ import Safe (readMay)
 import Text.Blaze.Html.Renderer.Text
 import Web.Alert
 import Yesod.Core
-import Yesod.Core.Handler
+import qualified Data.Text.Lazy as TL
 
--- Add an 'Alert'
-addAlert :: MonadHandler m => Alert -> m ()
-addAlert (Alert alert msg) = addMessage (pack $ show alert) $ toHtml msg
+-- Set an 'Alert'
+setAlert :: MonadHandler m => Alert -> m ()
+setAlert (Alert alert msg) = addMessage (pack $ show alert) $ toHtml msg
+
+setDefaultAlert :: MonadHandler m => TL.Text -> m ()
+setDefaultAlert msg = setAlert (Alert Default msg)
+
+setInfoAlert :: MonadHandler m => TL.Text -> m ()
+setInfoAlert msg = setAlert (Alert Info msg)
+
+setSuccessAlert :: MonadHandler m => TL.Text -> m ()
+setSuccessAlert msg = setAlert (Alert Success msg)
+
+setWarningAlert :: MonadHandler m => TL.Text -> m ()
+setWarningAlert msg = setAlert (Alert Warning msg)
+
+setErrorAlert :: MonadHandler m => TL.Text -> m ()
+setErrorAlert msg = setAlert (Alert Error msg)
 
 -- Get available 'Alert's
 getAlerts :: MonadHandler m => m [Alert]
