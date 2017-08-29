2c28e6844cdcf0bab358acfa5f00703a87c657ac
Daniel Campoverde <alx@sillybytes.net>
2017-08-29 17:34:45 -0500

    Foundation5 renderer

--- src/Yesod/Alert/Foundation5.hs
+++ src/Yesod/Alert/Foundation5.hs
@@ -1,10 +1,21 @@
-module Yesod.Alert.Bootstrap3
-    ( renderAlertsBootstrap3
+-- | Render 'Alert's using Foundation v5.x
+--
+--  Dependencies (These should be accessible in your app):
+--
+-- * Foundation's CSS and JS
+--
+-- * Modernizr
+--
+-- * JQuery
+
+module Yesod.Alert.Foundation5
+    ( renderAlertsFoundation5
+    , F5.AlertType(..)
     ) where
 
 import Text.Blaze
 import Web.Alert
-import qualified Web.Alert.Renderer.Bootstrap3 as B3
+import qualified Web.Alert.Renderer.Foundation5 as F5
 
-renderAlertsBootstrap3 :: [Alert] -> Markup
-renderAlertsBootstrap3 = preEscapedToMarkup . B3.renderAlertsBootstrap3
+renderAlertsFoundation5 :: F5.AlertType -> [Alert] -> Markup
+renderAlertsFoundation5 at as = preEscapedToMarkup $ F5.renderAlertsFoundation5 at as
